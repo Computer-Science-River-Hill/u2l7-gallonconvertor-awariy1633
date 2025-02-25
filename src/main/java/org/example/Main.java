ef8ee1fb041fcff2b024b132e2f7af6b827941fb
awariy1633 <awariy1633@inst.hcpss.org>
2025-02-25 21:26:06 +0000

    gcf

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -5,17 +5,14 @@ public class Main {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         
-        // Prompt user for the number of gallons
         System.out.print("Enter the number of gallons: ");
         double gallons = scanner.nextDouble();
         
-        // Conversion factors
         double quarts = gallons * 4;
         double pints = quarts * 2;
         double cups = pints * 2;
         double tablespoons = cups * 16;
         
-        // Display results
         System.out.println("\nIn " + gallons + " gallons there are:");
         System.out.println(quarts + " quarts");
         System.out.println(pints + " pints");
